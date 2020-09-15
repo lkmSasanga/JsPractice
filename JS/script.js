e06167abb682999eb86376522286304fe72a6c75
lkmSasanga <lkmsasanga@gmail.com>
2020-09-15 09:21:15 +0530

    Pass arguments via event listeners

--- JS/script.js
+++ JS/script.js
@@ -5,15 +5,17 @@ const ALERT = document.querySelector("#booking-alert");
 CTA.classList.remove("hide");
 ALERT.classList.add("hide");
 
-function reveal(e) {
+function reveal(e, current) {
     e.preventDefault();
-    CTA.classList.toggle("hide");
+    // CTA.classList.toggle("hide");
+    current.innerHTML == "Book Now!" ? CTA.innerHTML = "Ooops!" : CTA.innerHTML = "Book Now!";
     ALERT.classList.toggle("hide");
+    // 
 }
 
 // CTA.onclick = reveal;
 //to handle to onclick events we have to use 
 //onlciklistener event
 
-CTA.addEventListener("click", reveal, false);
+CTA.addEventListener("click", function(e) {reveal(e,this)}, false);
 CTA.addEventListener("click", function(){console.log("The BUtton was clicked!")});
